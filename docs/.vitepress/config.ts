671088f5d21cf08654fbc3a20d3bd0183f1665b8
Charles Wiltgen <charles@wiltgen.net>
2026-02-27 11:27:30 -0800

    chore: bump version to 2.30.1 (165 skills)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -512,7 +512,7 @@ export default withMermaid(defineConfig({
 
     footer: {
       message: 'Released under the MIT License',
-      copyright: 'Copyright © 2026 Charles Wiltgen • v2.30.0'
+      copyright: 'Copyright © 2026 Charles Wiltgen • v2.30.1'
     }
   }
 }))
